f58feb9f92c0b0f2420fa09884171e163af26230
Ben Frankel <ben.frankel7@gmail.com>
2018-06-17 17:34:49 -0400

    Fixed discover-my-mode keybinding, improved .gitignores

--- config/emacs/settings/setup-discover-my-major.el
+++ config/emacs/settings/setup-discover-my-major.el
@@ -1,6 +1,6 @@
 (use-package discover-my-major
   :bind (("C-h C-m" . discover-my-major)
-	 ("C-h M-m" . discover-my-major)))
+	 ("C-h M-m" . discover-my-mode)))
 
 (provide 'setup-discover-my-major)
 ;;; setup-discover-my-major.el ends here
